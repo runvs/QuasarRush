d0940ee9b41d372c6130d6cd001c3a2d0d5212fd
Thunraz <thunraz@thunraz.com>
2021-08-24 13:54:44 +0200

    Let player rotate

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -1,8 +1,8 @@
 #include "player.hpp"
 #include "drawable_helpers.hpp"
-#include "game_interface.hpp"
 #include "game.hpp"
-
+#include "game_interface.hpp"
+#include "math_helper.hpp"
 
 void Player::doCreate()
 {
@@ -10,20 +10,30 @@ void Player::doCreate()
     m_sprite->loadSprite("assets/ship.png");
     m_transform = std::make_shared<Transform>();
     m_transform->position = jt::Vector2 { 100, 100 };
-    m_transform->velocity= jt::Vector2 {20 , 20 };
+    m_transform->velocity = jt::Vector2 { 20, 20 };
 }
 
 void Player::doUpdate(float const elapsed)
 {
     float const a = getAge();
-    //m_transform->position = jt::Vector2(200 + sin(a / 2) * 50, 200 + cos(a / 2) * 50);
+    // m_transform->position = jt::Vector2(200 + sin(a / 2) * 50, 200 + cos(a / 2) * 50);
 
-    m_shape->setPosition(m_transform->position);
-    m_shape->update(elapsed);
+    if (getGame()->input()->keyboard()->pressed(jt::KeyCode::W)) { }
+
+    if (getGame()->input()->keyboard()->pressed(jt::KeyCode::A)) {
+        m_transform->angle += 36.0f;
+    } else if (getGame()->input()->keyboard()->pressed(jt::KeyCode::D)) {
+        m_transform->angle -= 36.0f;
+    }
+
+    m_sprite->setRotation(jt::MathHelper::deg2rad(m_transform->angle));
+
+    m_sprite->setPosition(m_transform->position);
+    m_sprite->update(elapsed);
 }
 
-void Player::doDraw() const { m_shape->draw(getGame()->getRenderTarget()); }
-void Player::doKill() {}
+void Player::doDraw() const { m_sprite->draw(getGame()->getRenderTarget()); }
+void Player::doKill() { }
 
 std::shared_ptr<Transform> Player::getTransform() { return m_transform; }
 void Player::setTransform(std::shared_ptr<Transform> t) { m_transform = t; }
--- src/game/transform.hpp
+++ src/game/transform.hpp
@@ -4,12 +4,12 @@
 #include "vector.hpp"
 
 struct Transform {
-    jt::Vector2 position{0.0f, 0.0f};
-    jt::Vector2 velocity {0.0f,0.0f};
-    jt::Vector2 acceleration {0.0f, 0.0f};
+    jt::Vector2 position { 0.0f, 0.0f };
+    jt::Vector2 velocity { 0.0f, 0.0f };
+    jt::Vector2 acceleration { 0.0f, 0.0f };
 
     /// in degree
-    float angle{0.0};
+    float angle { 0.0f };
 
     float mass { 1.0f };
 };
