26d5c1e63029db9118e8624468fd3d1a1a44c090
Thunraz <thunraz@thunraz.com>
2021-08-27 18:36:04 +0200

    Add enemy sprite

--- src/game/enemy.cpp
+++ src/game/enemy.cpp
@@ -1,11 +1,24 @@
 #include "enemy.hpp"
-#include "game_interface.hpp"
 #include "drawable_helpers.hpp"
+#include "game_interface.hpp"
 #include "game_properties.hpp"
+#include "math_helper.hpp"
 
 void Enemy::doCreate()
 {
-    m_shape = jt::dh::createRectShape(jt::Vector2{10.0f, 10.0f});
+    m_shipSprite = std::make_shared<jt::Animation>();
+    m_shipSprite->add("assets/enemy.png", "idle", jt::Vector2u { 16, 16 }, { 0 }, 0.15f);
+    m_shipSprite->add("assets/enemy.png", "fly", jt::Vector2u { 16, 16 }, { 1 }, 0.15f);
+    m_shipSprite->play("idle");
+    m_shipSprite->setOrigin(jt::Vector2 { 8.0, 8.0 });
+
+    m_flameSprite = std::make_shared<jt::Animation>();
+    m_flameSprite->add("assets/enemyFlame.png", "idle", jt::Vector2u { 16, 16 }, { 0 }, 0.15f);
+    m_flameSprite->add(
+        "assets/enemyFlame.png", "fly", jt::Vector2u { 16, 16 }, { 1, 2, 3, 4, 5 }, 0.15f);
+    m_flameSprite->play("idle");
+    m_flameSprite->setOrigin(jt::Vector2 { 8.0, 8.0 });
+
     m_transform = std::make_shared<Transform>();
 }
 
@@ -13,18 +26,18 @@ void Enemy::doUpdate(float const elapsed)
 {
     m_shootTimer -= elapsed;
 
-    m_shape->setOrigin(jt::Vector2 { 5.0, 5.0 });
-    m_shape->setRotation(m_transform->angle);
+    m_shipSprite->setRotation(m_transform->angle);
+    m_shipSprite->setPosition(m_transform->position);
+    m_shipSprite->update(elapsed);
 
-    m_shape->setPosition(m_transform->position);
-    m_shape->update(0.0f);
+    jt::Vector2 flameOffset { -8.0f, 0.0f };
+    flameOffset = jt::MathHelper::rotateBy(flameOffset, -m_transform->angle);
+    m_flameSprite->setPosition(m_shipSprite->getPosition() + flameOffset);
+    m_flameSprite->setRotation(m_transform->angle);
+    m_flameSprite->update(elapsed);
 }
 
-
-void Enemy::doDraw() const
-{
-    m_shape->draw(getGame()->getRenderTarget());
-}
+void Enemy::doDraw() const { m_shipSprite->draw(getGame()->getRenderTarget()); }
 void Enemy::doKill() { }
 
 std::shared_ptr<Transform> Enemy::getTransform() { return m_transform; }
@@ -33,10 +46,10 @@ void Enemy::setTransform(std::shared_ptr<Transform> t) { m_transform = t; }
 bool Enemy::canShoot() { return m_shootTimer <= 0; }
 void Enemy::shoot() { m_shootTimer = GP::EnemyShootTimer(); }
 
-void Enemy::takeDamage() {
+void Enemy::takeDamage()
+{
     m_health -= 1;
-    if (m_health <= 0)
-    {
+    if (m_health <= 0) {
         kill();
     }
 }
--- src/game/enemy.hpp
+++ src/game/enemy.hpp
@@ -1,12 +1,14 @@
 #ifndef QUASARRUSH_ENEMY_HPP
 #define QUASARRUSH_ENEMY_HPP
 
+#include "animation.hpp"
 #include "game_object.hpp"
 #include "shape.hpp"
 #include "transform.hpp"
 
 class Enemy : public jt::GameObject {
-    std::shared_ptr<jt::Shape> m_shape;
+    std::shared_ptr<jt::Animation> m_shipSprite;
+    std::shared_ptr<jt::Animation> m_flameSprite;
     std::shared_ptr<Transform> m_transform;
 
     float m_shootTimer = 0.0f;
@@ -26,7 +28,6 @@ public:
     void setTransform(std::shared_ptr<Transform> t);
 
     void takeDamage();
-
 };
 
 #endif // QUASARRUSH_ENEMY_HPP
