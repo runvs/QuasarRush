70d9c5360ec8d8e60288485b928ac3e3753c378d
Simon Weis <laguna_1989@gmx.net>
2021-09-12 08:52:13 +0200

    Clean up GameProperties

--- src/game/weapon_mg.cpp
+++ src/game/weapon_mg.cpp
@@ -13,14 +13,14 @@ void WeaponMg::shoot(jt::Vector2 const& shooterPos, jt::Vector2 const& mousePosi
         float shotTimeFactor = 1.0f - (static_cast<float>(playerConfig.weaponLevel) - 1.0f) * 0.1f;
         shotTimeFactor = jt::MathHelper::clamp(shotTimeFactor, 0.2f, 1.0f);
 
-        m_shootTimer = GP::PlayerShootTimerMg() * sqrt(shotTimeFactor);
+        m_shootTimer = GP::PlayerShootTimerMg() * static_cast<float>(sqrt(shotTimeFactor));
         jt::Vector2 const orthogonal_aim_direction { aimDirection.y(), -aimDirection.x() };
         auto startPos = shooterPos
             + 6.0f * orthogonal_aim_direction * ((m_shotCounter % 2 == 0) ? -1.0f : 1.0f);
         shotSpawnInterface.spawnShotMg(startPos, aimDirection, true);
 
         if (m_shotCounter >= GP::WeaponMgMagazineSize()) {
-            m_reloadTimer = 1.0f + GP::WeaponMgReloadTimer() * shotTimeFactor;
+            m_reloadTimer = 0.5f + GP::WeaponMgReloadTimer() * shotTimeFactor;
             m_reloadTimerMax = m_reloadTimer;
             m_shotCounter = 0;
         }
